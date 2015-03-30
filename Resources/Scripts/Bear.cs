4810e9986ca508866e8742585eadd885b0e138e2
Dave Yan <fdu5526@live.com>
2015-03-30 13:47:44 -0400

    starting character prefab, camera controls

---
+++ Resources/Scripts/Bear.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bear : MonoBehaviour {
+
+
+	public bool isOnTwoLegs;
+
+	// Use this for initialization
+	void Start () {
+		isOnTwoLegs = false;	
+	}
+
+
+
+	private void CheckControl()
+	{
+		if(isOnTwoLegs)		// two legs movement
+		{
+			if(Input.GetKey("w"))
+    	{ 
+      
+    	}
+    	if(Input.GetKey("a"))
+    	{ 
+      
+    	}
+    	if(Input.GetKey("s"))
+    	{ 
+      
+    	}
+    	if(Input.GetKey("d"))
+    	{ 
+      
+    	}
+		}
+		else							// four legs movement
+		{
+
+		}
+	}
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
