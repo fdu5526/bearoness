facf3badfcd440f5f0cba6d95a6d55b4517b407d
Dave Yan <fdu5526@live.com>
2015-04-08 16:25:28 -0400

    starting guard behavior

---
+++ Resources/Scripts/GuardDetectionCircle.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuardDetectionCircle : MonoBehaviour {
+
+	// the player
+	private GameObject bear;
+	private Bear bearScript;
+
+	// audio
+	private AudioSource[] audios;
+
+	// Use this for initialization
+	void Start () 
+	{
+		bear = GameObject.Find ("Bear");
+		bearScript = bear.GetComponent<Bear>();
+		//audios = GetComponents<AudioSource>();
+	}
+
+	void OnTriggerStay(Collider collider)
+	{	
+		if(collider.name.Equals("Bear"))
+		{
+			print("sdfsdf");
+		}
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+}
