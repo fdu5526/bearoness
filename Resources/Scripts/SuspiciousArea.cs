cf8c4c6d4157e05ef3bfb9dd1266598dde70ee51
Dave Yan <fdu5526@live.com>
2015-04-23 11:59:48 -0400

    implemented suspicious area

---
+++ Resources/Scripts/SuspiciousArea.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SuspiciousArea : MonoBehaviour {
+
+	// the bear script
+	private Bear bearScript;
+
+	// the amount to increase the suspicion when player is 4 legs
+	private const float suspicionIncreaseCount = 0.2f;
+
+	void Start()
+	{
+		GameObject bear = GameObject.Find ("Bear");
+		bearScript = bear.GetComponent<Bear>();
+	}
+
+
+	// enter the coat closet, time to reset
+	void OnTriggerStay(Collider collider)
+	{
+		if(collider.CompareTag("Bear"))
+		{
+			bearScript.IncreaseSuspicion(suspicionIncreaseCount);
+		}
+	}
+}
