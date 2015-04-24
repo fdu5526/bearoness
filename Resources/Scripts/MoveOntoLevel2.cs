3135f387e0c7b4e74f7aa29618f9d283a85273f5
Dave Yan <fdu5526@live.com>
2015-04-24 02:55:12 -0400

    added jank level transition to level2

---
+++ Resources/Scripts/MoveOntoLevel2.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveOntoLevel2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if(collision.gameObject.name.Equals("Bear"))
+		{
+			Application.LoadLevel ("level2");
+		}
+	}
+	
+
+}
