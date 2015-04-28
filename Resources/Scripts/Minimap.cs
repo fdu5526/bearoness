a3eac44789457191b36eb66f587f0aa5263f1565
Dave Yan <fdu5526@live.com>
2015-04-28 00:35:07 -0400

    starting minimap implementation

---
+++ Resources/Scripts/Minimap.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Minimap : MonoBehaviour {
+
+	private GameObject indicator;
+
+	// Use this for initialization
+	void Start () 
+	{
+		indicator = GetComponent<Transform>().Find("indicator").gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
