98cab34f5c5fbb08572d98dee6bf1fa992588baf
tgarncarz <trg@andrew.cmu.edu>
2015-04-29 16:02:01 -0400

    UI additions and changing objective text

--- Resources/Scripts/QuestBox.cs
+++ Resources/Scripts/QuestBox.cs
@@ -8,6 +8,8 @@ public class QuestBox : MonoBehaviour {
 	private GameObject bear, questBox;
 	private Bear bearScript;
 	private Image image;
+
+	private bool level2check1, level2check2;
 	Text questText;
 	string activeQuest = "Go to the Ballroom";
 
@@ -23,6 +25,9 @@ public class QuestBox : MonoBehaviour {
 		questText.text = activeQuest;
 
 		image.sprite = regbox;
+
+		level2check1 = true;
+		level2check2 = false;
 	}
 	
 	void ChangeQuest(string s){
@@ -35,6 +40,36 @@ public class QuestBox : MonoBehaviour {
 		{
 			image.sprite = suspbox;
 			ChangeQuest("Find a Changing Room!");
+		}
+
+		if (Application.loadedLevelName == "level2")
+		{
+			if (level2check1)
+			{
+				ChangeQuest("Go to the Kitchen");
+			}
+
+			else if (level2check2)
+			{
+				ChangeQuest("Find the rogue princess!");
+			}
+
+			if (bearScript.isDiscovered)
+			{
+				image.sprite = suspbox;
+				ChangeQuest("Find a Changing Room!");
+			}
+		}
+
+		if (Application.loadedLevelName == "level3")
+		{
+			ChangeQuest("Find the prince in the Ballroom");
+
+			if (bearScript.isDiscovered)
+			{
+				image.sprite = suspbox;
+				ChangeQuest("Find a Changing Room!");
+			}
 		}
 	}
 }
---
+++ Resources/Scripts/tutorialScript.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tutorialScript : MonoBehaviour {
+	private GameObject bear,twoLegsBox, detectedBox;
+	private Bear bearScript;
+
+	private bool isTriggered, isDiscovered, alreadyDiscovered;
+
+
+	// Use this for initialization
+	void Start () {
+		bear = GameObject.Find("Bear");
+		twoLegsBox = GameObject.Find("TwoLegsBox");
+		detectedBox = GameObject.Find("DetectedBox");
+		bearScript = bear.GetComponent<Bear>();
+		isTriggered = false;
+		alreadyDiscovered = false;
+		twoLegsBox.SetActive(false);
+		detectedBox.SetActive(false);
+	}
+
+	void OnTriggerEnter(Collider collision)
+	{
+		if(collision.gameObject.name.Equals("Bear"))
+		{
+			isTriggered = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		isDiscovered = GameObject.Find ("Bear").GetComponent<Bear>().isDiscovered;
+
+		if (isDiscovered && alreadyDiscovered == false)
+		{
+			detectedBox.SetActive(true);
+			Time.timeScale = 0;
+			alreadyDiscovered = true;
+		}
+
+		if (isTriggered)
+		{
+			twoLegsBox.SetActive(true);
+			Time.timeScale = 0;
+		}
+
+		if (Input.GetKeyDown("space") && twoLegsBox.activeInHierarchy)
+		{
+			twoLegsBox.SetActive(false);
+			isTriggered = false;
+			Destroy(this);
+			Time.timeScale = 1;
+		}
+
+		if (Input.GetKeyDown("space") && detectedBox.activeInHierarchy)
+		{
+			detectedBox.SetActive(false);
+			Time.timeScale = 1;
+		}
+
+
+	}
+}
