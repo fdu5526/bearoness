1df92b092f6baf2d75f55ea57adea6933b726807
Dave Yan <fdu5526@live.com>
2015-04-30 17:58:14 -0400

    golden chair in level1 only activate when not discovered now

--- Resources/Scripts/MoveOntoLevel2.cs
+++ Resources/Scripts/MoveOntoLevel2.cs
@@ -3,14 +3,18 @@ using System.Collections;
 
 public class MoveOntoLevel2 : MonoBehaviour {
 
+	private Bear bearScript;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		bearScript = GameObject.Find ("Bear").GetComponent<Bear>();
 	}
 
 	void OnCollisionEnter(Collision collision)
 	{
-		if(collision.gameObject.name.Equals("Bear"))
+		if(!bearScript.isDiscovered &&
+			collision.gameObject.name.Equals("Bear"))
 		{
 			Application.LoadLevel ("EatingContest");
 		}
