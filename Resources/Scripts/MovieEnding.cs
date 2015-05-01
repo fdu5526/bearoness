e7d1d31e9ed1c966d00b35a3fdb65766cd50a49f
Dave Yan <fdu5526@live.com>
2015-04-30 22:23:06 -0400

    finished credits

--- Resources/Scripts/MovieEnding.cs
+++ Resources/Scripts/MovieEnding.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class MovieEnding : MonoBehaviour {
 	
 	private MovieTexture movTexture;
+	private bool showCredits;
 
 	// Use this for initialization
 	void Start () 
 	{
+		showCredits = false;
 		movTexture = (MovieTexture)GetComponent<Renderer>().material.mainTexture;
 		movTexture.Play();
 		GetComponent<AudioSource>().Play();
@@ -16,7 +18,13 @@ public class MovieEnding : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		if(!movTexture.isPlaying)
+		if(!movTexture.isPlaying && !showCredits)
+		{
+			GetComponent<Renderer>().material = Resources.Load("UI/Materials/Credits", typeof(Material)) as Material;
+			showCredits = true;
+		}
+
+		if(showCredits && Input.anyKey)
 		{
 			Application.Quit();
 		}
